0f4155c0dd758c984e9474d02e9f9822daf43742
Anthony Rodriguez <arods642@gmail.com>
2018-10-22 16:06:35 -0700

    Working with Delete on API

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -10,20 +10,20 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',{ useNewUrlParser: true 
        return console.log('Unable to connect to MongoDB server.');
     }
     console.log('Connected to MongoDB server');
-    // const db = client.db('TodoApp')
-    // db.collection('Todos').insertOne({
-    //     text: 'Something to do',
-    //     completed: false
-    // }, (err, result) => {
-    //     if (err) {
-    //         return console.log('Unable to insert record into Todo', err)
-    //     }
+    const db = client.db('TodoApp')
+    db.collection('Todos').insertOne({
+        text: 'And Yet Another Item',
+        completed: false
+    }, (err, result) => {
+        if (err) {
+            return console.log('Unable to insert record into Todo', err)
+        }
 
-    //     console.log(JSON.stringify(result.ops, undefined, 2 ));
-    // });
+        console.log(JSON.stringify(result.ops, undefined, 2 ));
+    });
 
     // db.collection('Users').insertOne({
-    //     name: 'Sydney',
+    //     name: 'Anthony',
     //     age: 12,
     //     location: 'Los Angeles'
     // }, (err, result) => {
