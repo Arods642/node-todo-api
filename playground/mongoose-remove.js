b1d1aa9552bd3584dae48397e4c8440e4e46a58d
Anthony Rodriguez <arods642@gmail.com>
2018-11-02 15:31:30 -0700

    added Delete route.

--- playground/mongoose-remove.js
+++ playground/mongoose-remove.js
@@ -4,6 +4,16 @@ const {mongoose} = require('../server/db/mongoose');
 const {Todo} = require('../server/models/todo');
 const {User} = require('../server/models/user');
 
-Todo.remove({}).then((res) => {
-    console.log(results);
-})
+// Todo.remove({}).then((results) => {
+//     console.log(results);
+// });
+
+Todo.findOneAndRemove({_id: '5bdb45ee2e5ed232833491be'}).then((todo) => {
+    console.log(todo);
+});
+
+
+// Todo.findByIdAndRemove('5bdb446a2e5ed232833491b1').then((todo) => {
+//     console.log(todo);
+// });
+
